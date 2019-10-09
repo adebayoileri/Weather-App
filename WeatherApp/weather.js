66f4ab3a5d76208cf65ca4b1a0e0efee3d3cce26
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-10-09 17:26:49 +0100

    Implemented local storage
    saves city requested to oocal storage for  better user experience

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -18,7 +18,6 @@ class UI{
         this.desc = document.getElementById('w-desc-info');
         this.temp = document.getElementById('w-temp-info');
         this.time = document.getElementById('w-time');
-        this.icon = document.getElementById('w-icon');
         this.humidity = document.getElementById('w-humidity');
         this.dewpoint = document.getElementById('w-dew-point');
         this.feels = document.getElementById('w-feels');
@@ -26,22 +25,46 @@ class UI{
     }
     paint(weather){
         this.location.textContent =`${weather.name} ,${weather.sys.country}`;
-        this.desc.textContent = `${weather.weather[0].description}`;
-        this.wind.textContent =`Wind Speed : ${weather.wind.speed*3.6}km/hr`;
+        this.desc.textContent = `${weather.weather[0].main}`;
+        this.wind.textContent =`Wind Speed : ${Math.floor(weather.wind.speed*3.6)} km/hr`;
         this.humidity.textContent =`Relative Humidity :${weather.main.humidity}%`;
         this.feels.textContent =`Pressure: ${weather.main.pressure}hPa`;
-        this.dewpoint.textContent =`Temperature ranges from ${weather.main.temp_min - 273.15}°C
-        to ${weather.main.temp_max - 273.15}°C
+        this.dewpoint.textContent =`Temperature ranges from ${Math.floor(weather.main.temp_min - 273.15)}°C
+        to ${Math.floor(weather.main.temp_max - 273.15)}°C
         `; 
         this.time.textContent = new Date().toDateString();
-        this.temp.textContent = `${weather.main.temp}K (${weather.main.temp - 273.15}°C) `
-        this.icon.setAttribute('class',weather.weather[0].icon);
+        this.temp.textContent = `${weather.main.temp}K (${Math.floor(weather.main.temp - 273.15)}°C) `
     }
 }
-// Initialized Class Weather
-const weather =new Weather('Lagos');
 
-const ui = new UI();
+class Storage{
+    constructor(){
+    this.city;
+    this.defaultCity = 'Lagos';    
+    }
+
+    getLocationData(){
+        if(localStorage.getItem('city') === null){
+            this.city = this.defaultCity;
+        }else{
+            this.city =localStorage.getItem('city');
+        }
+        return{
+            city:this.city
+        }
+    }
+    setLocationData(city){
+        localStorage.setItem('city',city);
+    }
+
+}
+// Initialized Class Weather
+const storage =new Storage();
+const weatherLocation = storage.getLocationData();
+
+const weather =new Weather(weatherLocation.city);
+
+const ui =new UI();
 
 //Get Default Weather
 const getWeather = () => weather.getWeather()
@@ -52,4 +75,16 @@ const getWeather = () => weather.getWeather()
         console.log(err);
     });
 
-    window.addEventListener('DOMContentLoaded',getWeather);
+    window.addEventListener('DOMContentLoaded',getWeather());
+
+    // Change Location
+    document.getElementById('change-weather').addEventListener('click',(e) => {
+        e.preventDefault();
+        const city = document.getElementById('w-city').value
+        weather.changeLocation(city);
+        storage.setLocationData(city);
+        getWeather();
+        const modal = document.getElementById('location-modal');
+        modal.style.display ="none"
+    });
+    
